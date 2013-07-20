b412c4a15087ed07e8ec7b974da4ba21fcdd4799
gscoop <lanes@georgestreetcoop.com>
2013-07-19 21:00:56 -0400

    added logs for PHP errors and MySQL queries

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-sudo -s
-sudo ln -s ./ini.php /CORE-POS/pos/is4c-nf/ini.php
-sudo ln -s ./ini-local.php /CORE-POS/pos/is4c-nf/ini-local.php
+
+touch "/CORE-Support/ini.php"
+ln -s "/CORE-Support/ini.php" "/CORE-POS/pos/is4c-nf/ini.php"
+
+touch "/CORE-Support/ini-local.php"
+ln -s "/CORE-Support/ini-local.php" "/CORE-POS/pos/is4c-nf/ini-local.php"
+
+touch "/CORE-POS/pos/is4c-nf/log/php-errors.log"
+chown www-data "/CORE-POS/pos/is4c-nf/log/php-errors.log"
+
+touch "/CORE-POS/pos/is4c-nf/log/queries.log"
+chown www-data "/CORE-POS/pos/is4c-nf/log/queries.log"
+
 echo Done
