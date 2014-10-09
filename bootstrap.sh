00a8c9bbfdd172591fd3db88ea22b9513a3a99cc
gscoop <lanes@georgestreetcoop.com>
2014-10-09 18:03:52 -0400

    Cleanup and commenting.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,42 +1,49 @@
 #!/bin/sh
-
 # run this script under sudo!
 
-apt-get install git
-
+# set up variables
 COREPOS=/CORE-POS
 SUPPORT=/CORE-Support
 LANEIDFILE=$SUPPORT/laneid.txt
 
-rm -rf $SUPPORT
-mkdir $SUPPORT
-cd $SUPPORT
-git clone https://github.com/GeorgeStreetCoop/CORE-Support.git $SUPPORT
-chown -Rf coop $SUPPORT
+# bootstrap git
+apt-get install git
 
+# get latest Support directory
+rm -rf "$SUPPORT"
+mkdir "$SUPPORT"
+cd "$SUPPORT"
+git clone https://github.com/GeorgeStreetCoop/CORE-Support.git "$SUPPORT"
+chown -Rf coop "$SUPPORT"
+
+# install needed packages
 . ./apt-updates.sh
 
+# get/set lane ID
 if [ -f "$LANEIDFILE" ]; then
-	LANEID=`cat $LANEIDFILE`
+	LANEID=`cat "$LANEIDFILE"`
 fi
 if [ -z "$LANEID" ]; then
 	while [ -z "$LANEID" ]; do
 		read -p "What is the name of this POS lane? (lane1, lane2, etc) " LANEID
 	done
-	echo -n $LANEID > $LANEIDFILE
+	echo -n "$LANEID" > "$LANEIDFILE"
 fi
-
 echo "Lane ID is $LANEID, stored at $LANEIDFILE"
-ls -l $LANEIDFILE
+ls -l "$LANEIDFILE"
 
-rm -rf $COREPOS
-mkdir $COREPOS
-cd $COREPOS
-git clone https://github.com/CORE-POS/IS4C.git $COREPOS
-chown -Rf coop $COREPOS
+# get latest POS directory
+rm -rf "$COREPOS"
+mkdir "$COREPOS"
+cd "$COREPOS"
+git clone https://github.com/CORE-POS/IS4C.git "$COREPOS"
+chown -Rf coop "$COREPOS"
 
-touch "$COREPOS/pos/is4c-nf/log/php-errors.log"
-chown www-data "$COREPOS/pos/is4c-nf/log/php-errors.log"
+# set up our ini files
+cp "$SUPPORT/ini.php" "$COREPOS/pos/is4c-nf/ini.php"
+cp "$SUPPORT/ini-local.php" "$COREPOS/pos/is4c-nf/ini-local.php"
+chown www-data "$COREPOS/pos/is4c-nf/ini.php" "$COREPOS/pos/is4c-nf/ini-local.php"
 
-touch "$COREPOS/pos/is4c-nf/log/queries.log"
-chown www-data "$COREPOS/pos/is4c-nf/log/queries.log"
+# set up error logs
+touch "$COREPOS/pos/is4c-nf/log/php-errors.log" "$COREPOS/pos/is4c-nf/log/queries.log"
+chown www-data "$COREPOS/pos/is4c-nf/log/php-errors.log" "$COREPOS/pos/is4c-nf/log/queries.log"
