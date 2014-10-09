8464d72118dff8c6df8174f0c3e280cdcff9eafc
gscoop <lanes@georgestreetcoop.com>
2014-10-09 10:26:37 -0400

    added apt-get install for wget and unzip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# run this script under sudo!
+
+apt-get install wget unzip
 wget https://github.com/GeorgeStreetCoop/CORE-Support/archive/master.zip -O ~/CORE-Support.zip
 unzip ~/CORE-Support.zip -d /CORE-Support
 
