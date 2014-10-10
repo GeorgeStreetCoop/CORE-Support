07b0fad3fa8ba280ac73c4a5b4b6397e32bf682e
gscoop <lanes@georgestreetcoop.com>
2014-10-10 01:19:17 -0400

    Set up openbox autolaunch.

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,3 +67,12 @@ sed -i 's/alias firefox=.+//;s/alias geany=.+//;s/alias smartgit=.+//' ~coop/.ba
 sed -i '$a alias firefox="firefox >/dev/null 2>&1 &"' ~coop/.bashrc
 sed -i '$a alias geany="geany >/dev/null 2>&1 &"' ~coop/.bashrc
 sed -i '$a alias smartgit="smartgithg >/dev/null 2>&1 &"' ~coop/.bashrc
+
+#set up openbox autolaunch
+mkdir -p ~coop/.config/openbox
+touch ~coop/.config/openbox/autostart
+sed -i 's/^xterm.+//;s/^firefox.+//;s/^geany.+//;s/^smartgit.+//' ~coop/.config/openbox/autostart
+sed -i '$a xterm >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
+sed -i '$a firefox >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
+sed -i '$a geany >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
+sed -i '$a smartgithg >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
