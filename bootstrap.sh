b3c782412a0fbef3a16472d2e5c42e93ba675668
John Leary <john.leary@gmail.com>
2014-10-10 02:49:04 -0400

    GRUB_CMDLINE_LINUX not GRUB_CMDLINE_LINUX_DEFAULT

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,7 @@ chown www-data "$COREPOS/pos/is4c-nf/log/php-errors.log" "$COREPOS/pos/is4c-nf/l
 
 
 # set up grub boot process
-sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*quiet\|splash.*/s/^GRUB_CMDLINE_LINUX_DEFAULT=\(.*\)\(quiet\|splash\)\(.*\)\(quiet\|splash\)\(.*\)$/# was &\nGRUB_CMDLINE_LINUX_DEFAULT=\1\3\5/' /etc/default/grub
+sed -i '/^GRUB_CMDLINE_LINUX=.*quiet\|splash.*/s/^GRUB_CMDLINE_LINUX=\(.*\)\(quiet\|splash\)\(.*\)\(quiet\|splash\)\(.*\)$/# was &\nGRUB_CMDLINE_LINUX=\1\3\5/' /etc/default/grub
 sed -i 's/.*GRUB_INIT_TUNE=.*/GRUB_INIT_TUNE="480 440 1 660 1 880 1 660 1 440 3"/' /etc/default/grub
 update-grub
 
