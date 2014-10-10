6efd0fc82ae2c61d7a913597ad67bad9929b38da
gscoop <lanes@georgestreetcoop.com>
2014-10-09 21:43:31 -0400

    Now links to ini files instead of copying.

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,9 +40,9 @@ git clone https://github.com/CORE-POS/IS4C.git "$COREPOS"
 chown -Rf coop "$COREPOS"
 
 # set up our ini files
-cp "$SUPPORT/ini.php" "$COREPOS/pos/is4c-nf/ini.php"
-cp "$SUPPORT/ini-local.php" "$COREPOS/pos/is4c-nf/ini-local.php"
-chown www-data "$COREPOS/pos/is4c-nf/ini.php" "$COREPOS/pos/is4c-nf/ini-local.php"
+ln -svf "$SUPPORT/ini.php" "$COREPOS/pos/is4c-nf/ini.php"
+ln -svf "$SUPPORT/ini-local.php" "$COREPOS/pos/is4c-nf/ini-local.php"
+chown www-data "$SUPPORT/ini.php" "$SUPPORT/ini-local.php"
 
 # set up error logs
 touch "$COREPOS/pos/is4c-nf/log/php-errors.log" "$COREPOS/pos/is4c-nf/log/queries.log"
