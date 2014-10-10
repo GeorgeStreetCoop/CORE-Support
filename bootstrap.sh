364dd8a491465fec127afc3255cc301e0e3584d2
gscoop <lanes@georgestreetcoop.com>
2014-10-10 10:42:52 -0400

    ini file handling: ini.php is linked (edits reflect back to shared codebase), ini-local.php is copied (edits don't automatically share).

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,11 +39,13 @@ cd "$COREPOS"
 git clone https://github.com/CORE-POS/IS4C.git "$COREPOS"
 chown -Rf coop "$COREPOS"
 
-# set up our ini files
+# set up our ini files:
+# ini.php is linked, so changes reflect back up to the shared codebase
 ln -svf "$SUPPORT/ini.php" "$COREPOS/pos/is4c-nf/ini.php"
-ln -svf "$SUPPORT/ini-local.php" "$COREPOS/pos/is4c-nf/ini-local.php"
-chown www-data "$SUPPORT/ini.php" "$SUPPORT/ini-local.php"
-chown www-data "$COREPOS/pos/is4c-nf/ini.php" "$COREPOS/pos/is4c-nf/ini-local.php"
+chown www-data "$SUPPORT/ini.php" "$COREPOS/pos/is4c-nf/ini.php"
+# ini-local.php is copied, so local changes don't automatically share
+cp "$SUPPORT/ini-local.php" "$COREPOS/pos/is4c-nf/ini-local.php"
+chown www-data "$COREPOS/pos/is4c-nf/ini-local.php"
 
 # set up error logs
 touch "$COREPOS/pos/is4c-nf/log/php-errors.log" "$COREPOS/pos/is4c-nf/log/queries.log"
