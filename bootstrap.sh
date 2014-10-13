98106fef6e85b1fe19ff278f711ace4612ce88f2
John Leary <john.leary@gmail.com>
2014-10-13 11:57:08 -0400

    Eliminate sed for appends
    Now using 'echo XXX >> YYY' form (instead of sed '$a') to append to ~/.config/openbox/autostart, ~/.bashrc, and ~/.mozilla/firefox/*/prefs.js.

--- bootstrap.sh
+++ bootstrap.sh
@@ -94,20 +94,20 @@ sed -i '/skip-networking/s/^\( *skip-networking\)/# \1/' /etc/mysql/my.cnf
 su coop
 xdg-settings set default-web-browser firefox.desktop
 sed -i '/"browser.startup.homepage"/d' ~coop/.mozilla/firefox/*/prefs.js
-sed -i '$a user_pref("browser.startup.homepage", "http://localhost/POS/install/index.php");' ~coop/.mozilla/firefox/*/prefs.js
+echo 'user_pref("browser.startup.homepage", "http://localhost/POS/install/index.php");' >> ~coop/.mozilla/firefox/*/prefs.js
 
 # set up bash aliases
 touch ~coop/.bashrc
-sed -i '/alias firefox=/d;/alias geany=/d;/alias smartgit=/d' ~coop/.bashrc
-sed -i '$a alias firefox="nohup firefox >/dev/null 2>&1 &"' ~coop/.bashrc
-sed -i '$a alias geany="nohup geany >/dev/null 2>&1 &"' ~coop/.bashrc
-sed -i '$a alias smartgit="nohup smartgithg >/dev/null 2>&1 &"' ~coop/.bashrc
+sed -i '/alias firefox=/d;/alias geany=/d;/alias smartgit=/d' >> ~coop/.bashrc
+echo 'alias firefox="nohup firefox >/dev/null 2>&1 &"' >> ~coop/.bashrc
+echo 'alias geany="nohup geany >/dev/null 2>&1 &"' >> ~coop/.bashrc
+echo 'alias smartgit="nohup smartgithg >/dev/null 2>&1 &"' >> ~coop/.bashrc
 
 # set up openbox autolaunch
 mkdir -p ~coop/.config/openbox
 touch ~coop/.config/openbox/autostart
 sed -i '/xterm/d;/firefox/d;/geany/d;/smartgit/d' ~coop/.config/openbox/autostart
-sed -i '$a xterm >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
-sed -i '$a firefox >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
-sed -i '$a geany >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
-sed -i '$a smartgithg >/dev/null 2>&1 &' ~coop/.config/openbox/autostart
+echo 'xterm >/dev/null 2>&1 &' >> ~coop/.config/openbox/autostart
+echo 'firefox >/dev/null 2>&1 &' >> ~coop/.config/openbox/autostart
+echo 'geany >/dev/null 2>&1 &' >> ~coop/.config/openbox/autostart
+echo 'smartgithg >/dev/null 2>&1 &' >> ~coop/.config/openbox/autostart
