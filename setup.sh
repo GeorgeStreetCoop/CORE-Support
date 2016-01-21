e547357828a01212eef0e23ca1e15216862cd684
John Leary <john.leary@gmail.com>
2016-01-20 21:39:41 -0500

    Remove GeorgeStreetCoop/CORE-POS branch confusion

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ if [ -n "$1" -a "$1" = "rm" ]; then
 	rm -rf "$COREPOS"
 	mkdir -p "$COREPOS" 2>/dev/null
 	cd "$COREPOS"
-	git clone https://github.com/CORE-POS/IS4C.git --branch version-1.9 "$COREPOS"
+	git clone https://github.com/CORE-POS/IS4C.git "$COREPOS"
 else
 	cd "$COREPOS"
 	git reset --hard HEAD
