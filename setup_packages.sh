0685f82fbbf8d46fc4eb8d976a0f8c66851f7209
John Leary <john.leary@gmail.com>
2017-06-11 16:16:54 -0400

    Autoremove unneeded packages

--- setup_packages.sh
+++ setup_packages.sh
@@ -15,6 +15,7 @@ sudo apt-get -y install `cat packages.txt`
 
 # add packages
 sudo apt-get -y remove `cat packages_remove.txt`
+sudo apt-get -y autoremove
 
 # update to latest versions
 sudo apt-get -y upgrade
