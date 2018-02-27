e38f158c30fe817facc75cfddd24d2bf97044175
John Leary <john.leary@gmail.com>
2018-02-27 18:01:40 -0500

    Replace firefox with surf

--- setup_user.sh
+++ setup_user.sh
@@ -14,10 +14,8 @@ fi
 
 # set up bash aliases
 touch ~/.bashrc
-sed -i '/alias firefox=/d;/alias geany=/d;/alias smartgit=/d;/alias r=/d;/alias k=/d;/alias kk=/d;/alias c=/d;/alias nv1=/d;/alias log_cl=/d;/alias log_pr=/d;/S ()/d;' ~/.bashrc
-echo 'alias firefox="nohup firefox >/dev/null 2>&1 &"' >> ~/.bashrc
-echo 'alias geany="nohup geany >/dev/null 2>&1 &"' >> ~/.bashrc
-echo 'alias smartgit="nohup smartgithg >/dev/null 2>&1 &"' >> ~/.bashrc
+sed -i '/alias firefox=/d;/alias surf=/d;/alias geany=/d;/alias smartgit=/d;/alias r=/d;/alias k=/d;/alias kk=/d;/alias c=/d;/alias nv1=/d;/alias log_cl=/d;/alias log_pr=/d;/S ()/d;' ~/.bashrc
+echo 'alias surf="nohup surf localhost/lane >/dev/null 2>&1 &"' >> ~/.bashrc
 echo "alias r=\"date +'%b %d %l:%M:%S'; $SUPPORT/reset_live.sh\"" >> ~/.bashrc
 echo 'alias k="date +\"%b %d %l:%M:%S\"; echo -en \\\x1Bp0~~ >/dev/ttyS1"' >> ~/.bashrc
 echo 'alias kk="date +\"%b %d %l:%M:%S\"; echo -en \\\x10\\\x14\\\x01\\\x00\\\x08 >/dev/ttyS1"' >> ~/.bashrc
@@ -30,20 +28,11 @@ echo 'S () { echo "S$@" > /dev/ttyS0; }' >> ~/.bashrc
 # set up openbox autolaunch
 mkdir -p ~/.config/openbox
 touch ~/.config/openbox/autostart
-sed -i '/xterm/d;/firefox/d;/geany/d;/smartgit/d;/xrandr/d;/numlockx/d;' ~/.config/openbox/autostart
+sed -i '/xterm/d;/firefox/d;/surf/d;/geany/d;/smartgit/d;/xrandr/d;/numlockx/d;' ~/.config/openbox/autostart
 echo 'xterm >/dev/null 2>&1 &' >> ~/.config/openbox/autostart
-echo 'firefox >/dev/null 2>&1 &' >> ~/.config/openbox/autostart
-# echo 'geany >/dev/null 2>&1 &' >> ~/.config/openbox/autostart
-# echo 'smartgithg >/dev/null 2>&1 &' >> ~/.config/openbox/autostart
+echo 'surf localhost/lane >/dev/null 2>&1 &' >> ~/.config/openbox/autostart
 echo 'xrandr --output VGA1 --mode 1024x768 --rate 75' >> ~/.config/openbox/autostart
 echo '[ -x /usr/bin/numlockx ] && /usr/bin/numlockx on' >> ~/.config/openbox/autostart
 
 # set up git credentials
 cp "$SUPPORT/template.gitconfig" ~/.gitconfig
-
-# set up browser
-/usr/bin/firefox -setDefaultBrowser 2>/dev/null &
-sleep 10
-kill `pidof firefox` 2>/dev/null
-sed -i '/"browser.startup.homepage"/d' ~/.mozilla/firefox/*/prefs.js
-echo 'user_pref("browser.startup.homepage", "http://localhost/lane/install/index.php");' >> ~/.mozilla/firefox/*/prefs.js
