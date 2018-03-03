a61fbc23db73d73e7fce2642d863c812284f63fd
John Leary <john.leary@gmail.com>
2018-03-03 11:15:49 -0500

    Specify pathname for opdata_tables.txt

--- refresh_opdata.sh
+++ refresh_opdata.sh
@@ -2,6 +2,12 @@
 # run this script under sudo!
 
 
+# set up context
+if [ -z "$SUPPORT" ]; then
+	SUPPORT=/CORE-Support
+fi
+
+
 # create base tables
 echo "Running base install page to create tables..."
 wget -O /dev/null -q "http://localhost/lane/install/index.php"
@@ -12,4 +18,4 @@ while read tablename
 do
     echo "Syncing $tablename..."
     wget -O /dev/null -q "http://rambutan/office/sync/TableSyncPage.php?tablename=&othertable=$tablename"
-done < opdata_tables.txt
+done < "$SUPPORT/opdata_tables.txt"
