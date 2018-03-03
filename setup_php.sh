26e64a50069801c4e5d034ff2facebff51c837f0
John Leary <john.leary@gmail.com>
2018-03-02 19:19:47 -0500

    Handle php.ini locations for both CLI and FPM, and both PHP5 and PHP7

--- setup_php.sh
+++ setup_php.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run this script under sudo!
 
+
 # set up PHP php.ini
-sed -i 's/^short_open_tag = Off/short_open_tag = On/' /etc/php5/cli/php.ini
+sed -i 's/^short_open_tag = Off/short_open_tag = On/' /etc/php*/*/php.ini /etc/php/*/*/php.ini 2&>/dev/null
