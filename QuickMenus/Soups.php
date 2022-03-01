71c67986be47d78a8ccf7ba333363b407888e749
John Leary <john.leary@gmail.com>
2022-03-01 00:06:23 -0500

    bye Black Bean, Chickpea Lentil, Split Pea Kale, White Bean Escarole; hello Chipotle Sweet Potato

--- QuickMenus/Soups.php
+++ QuickMenus/Soups.php
@@ -1,24 +1,18 @@
 <?php
 
 $my_menu = array(
-		'Black Bean Soup, 12oz' => '223',
-		'Black Bean Soup, 16oz' => '224',
-		'Chickpea Lentil Soup, 12oz' => '233',
-		'Chickpea Lentil Soup, 16oz' => '234',
+		'Chipotle Sweet Potato Soup, 12oz' => '283',
+		'Chipotle Sweet Potato Soup, 16oz' => '284',
 		'Lentil Soup, 12oz' => '123',
 		'Lentil Soup, 16oz' => '124',
 		'Quinoa Sweet Potato Chili w/ Black Beans, 12oz' => '273',
 		'Quinoa Sweet Potato Chili w/ Black Beans, 16oz' => '274',
 		'Roasted Vegetable Soup, 12oz' => '133',
 		'Roasted Vegetable Soup, 16oz' => '134',
-		'Split Pea & Kale Soup, 12oz' => '253',
-		'Split Pea & Kale Soup, 16oz' => '254',
 		'Three Bean Chili, 12oz' => '263',
 		'Three Bean Chili, 16oz' => '264',
 		'Tomato Soup w/ Garden Vegetables, 12oz' => '143',
 		'Tomato Soup w/ Garden Vegetables, 16oz' => '144',
-		'White Bean & Escarole Soup, 12oz' => '153',
-		'White Bean & Escarole Soup, 16oz' => '154',
 
 		'(other budget soup), 12oz' => '103',
 		'(other budget soup), 16oz' => '104',
