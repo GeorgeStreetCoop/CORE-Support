85fa71754a671babd5d863356868b6425f83a5fb
John Leary <john.leary@gmail.com>
2023-07-30 16:56:18 -0400

    FIX: www.conf comment char is ";", not "#"

--- setup_php.sh
+++ setup_php.sh
@@ -7,7 +7,7 @@ sed -i 's/^short_open_tag = Off/short_open_tag = On/' /etc/php*/*/php.ini /etc/p
 
 
 # back up PHP FPM configs, then increase pool size to prevent "server reached pm.max_children setting" error
-find /etc/php/*/fpm -name www.conf -exec sh -c 'cp -n $0 $0~ ; sed -i "s/^\s*#*\s*pm.max_children\s*=\s*5\s*#*/pm.max_children\t= 25 # CORE-Support setup_php.sh/" $0' {} \;
+find /etc/php/*/fpm -name www.conf -exec sh -c 'cp -n $0 $0~ ; sed -i "s/^\s*#*\s*pm.max_children\s*=\s*5\s*#*/pm.max_children\t= 25 ; CORE-Support setup_php.sh/" $0' {} \;
 
 
 # restart nginx and PHP FPM
